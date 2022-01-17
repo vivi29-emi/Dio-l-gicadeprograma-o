10a7cbe8f89efb7f0a8d6a92b545b3c16d9342ac
Viviane Emilie Geraldo <vivi29emi@gmail.com>
2022-01-17 17:21:15 -0300

    primeiro commit das atividade de lógica de programação

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//* definindo variaveis e constante com concatenação
+let nota1=7
+let nota2=3
+let nota3=10
+let nota4=5
+const media=(nota1+nota2+nota3+nota4)/4
+
+if (media>=7) { console.log(` você passou de ano!${media}`)
+    
+}
+else{ console.log(` você não passou de ano!${media}`)
+
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
